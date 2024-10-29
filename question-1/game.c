c4fd43b7ec37da5ce404f34c795d005b941c85db
Gabriel Opiyo <160593612+GabrielNyawade@users.noreply.github.com>
2024-10-29 12:37:01 +0300

    Objective for the code

---
+++ question-1/game.c
@@ -0,0 +1,14 @@
+/* QUESTION:
+ *
+ * A program is required for a computer game. The user keys in the number of
+ * rounds he wishes to play. For each round the user enters his lucky number.
+ * The program takes the number and divides it with a secret number. If the
+ * remainder of the division is zero, it is considered a draw for the round and
+ * the total score is incriminated by 1. Otherwise if it is any other even
+ * number, it is any other even number, it is considered a win for the round and
+ * the total score is incremented by 3. However, if it is an odd number, it is
+ * considered a loss for the round and the total score is decremented by 3. This
+ * is done until he completes his rounds. He wins if the total at score at the
+ * end is a positive number. Write a C program to accomplish this.
+ *
+ */
